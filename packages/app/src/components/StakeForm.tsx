7a17a4884d6bc606c01dac5e394682e85f5c0553
Thomas Gehrmann <s6thgehr@gmail.com>
2023-01-14 17:05:42 +0100

    Implement max button in AmountInput

--- packages/app/src/components/StakeForm.tsx
+++ packages/app/src/components/StakeForm.tsx
@@ -1,6 +1,7 @@
 import BN from "bn.js";
 import React, { useState } from "react";
 import { FiArrowDownLeft, FiArrowUpRight } from "react-icons/fi";
+import clx from "classnames";
 
 import DepositWarningModal from "./modals/DepositWarningModal";
 import useModal from "../hooks/useModal";
@@ -11,34 +12,72 @@ import { toFixedWithPrecision, toSol } from "../lib/util";
 interface AmountInputProps {
   className?: string;
   balance: BN | undefined;
+  amount: string;
   setAmount: Function;
 }
 
 const AmountInput: React.FC<AmountInputProps> = ({
   balance,
   className,
+  amount,
   setAmount,
-}) => (
-  <div className={className}>
-    <div className="flex flex-row justify-between p-8 my-auto bg-background">
-      <img src="solana-logo.png" className="h-24 my-auto" />
-      <div className="my-auto">
-        <div className="text-right">
-          Balance:{" "}
-          <span className="text-blue">
-            {balance ? toFixedWithPrecision(toSol(balance)) : "-"} SOL
+}) => {
+  const [isMax, setIsMax] = useState(false);
+
+  return (
+    <div className={className}>
+      <div className="flex flex-row justify-between p-8 my-auto bg-background">
+        <img src="solana-logo.png" className="h-24 my-auto" />
+        <div className="my-auto">
+          <div className="text-right">
+            Balance:{" "}
+            <span className="text-blue">
+              {balance ? toFixedWithPrecision(toSol(balance)) : "-"} SOL
+            </span>
+          </div>
+          <span className="flex flex-row mt-2">
+            <button
+              className={clx(
+                "text-xl my-auto mx-4 px-4 py-1 border-solid border-[1px] border-green rounded-md bg-green",
+                { "bg-opacity-25": !isMax }
+              )}
+              onClick={() => {
+                setIsMax((prevState) => {
+                  if (prevState) {
+                    // setAmount here creates a warning but I don't understand why, it says this is a bad setState call
+                    setAmount("0");
+                  } else {
+                    balance &&
+                      setAmount(
+                        toFixedWithPrecision(toSol(balance)).toString()
+                      );
+                  }
+                  return !prevState;
+                });
+              }}
+            >
+              Max
+            </button>
+
+            <input
+              className="w-28 border-none bg-transparent text-3xl text-right"
+              type="number"
+              min="0"
+              // It still show comma instead of point for decimal
+              lang="en"
+              placeholder="0.00"
+              value={amount}
+              onChange={(ev) => {
+                setAmount(ev.currentTarget.value);
+                setIsMax(false);
+              }}
+            />
           </span>
         </div>
-        <input
-          className="w-full border-none bg-transparent text-3xl text-right"
-          type="number"
-          placeholder="0.00"
-          onChange={(ev) => setAmount(ev.currentTarget.value)}
-        />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 interface StakeFormProps {
   withdraw: (amount: string) => void;
@@ -77,6 +116,7 @@ const StakeForm: React.FC<StakeFormProps> = ({
       <AmountInput
         className="mb-5"
         balance={solBalance}
+        amount={amount}
         setAmount={setAmount}
       />
       <div className="flex items-center justify-end">
