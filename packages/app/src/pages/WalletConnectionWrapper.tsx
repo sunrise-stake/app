e954ba73f25e50d187e8dd39d24d3d9775cd58be
Ronny Haase <ronnyhaase@fastmail.com>
2023-01-16 23:16:31 +0100

    Add minor UI improvements
    - Adaptions for mobile
    - Animate leaf icons on buttons
    - Fix button sizes

--- packages/app/src/pages/WalletConnectionWrapper.tsx
+++ packages/app/src/pages/WalletConnectionWrapper.tsx
@@ -10,23 +10,25 @@ export const WalletConnectionWrapper: FC = () => {
   const wallet = useWallet();
   return (
     <div className="min-h-full w-full flex flex-col items-center">
-      <div className="container flex mt-12 mb-4">
+      <header className="container flex mt-12 mb-4 px-8">
         <div className="grow text-3xl">
           <img
-            className="w-auto h-16 mr-7 inline"
+            className="hidden sm:block w-auto h-16"
             src={"./logo.png"}
             alt="Sunrise"
           />
         </div>
         <div>
           <WalletMultiButton startIcon={<FaWallet size={"28px"} />}>
-            {!wallet.connected ? "Connect Wallet" : null}
+            {!wallet.connected ? (
+              <div className="hidden sm:block ml-2">Connect Wallet</div>
+            ) : null}
           </WalletMultiButton>
         </div>
-      </div>
-      <div className="container mx-auto px-4">
-        <div>{wallet.connected ? <StakeDashboard /> : <WelcomePage />}</div>
-      </div>
+      </header>
+      <main className="container mx-auto px-8">
+        <>{wallet.connected ? <StakeDashboard /> : <WelcomePage />}</>
+      </main>
     </div>
   );
 };
