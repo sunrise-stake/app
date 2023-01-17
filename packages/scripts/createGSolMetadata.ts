834f53577e9cb9e22f383c5d3bf27ff43a7b7054
Thomas Gehrmann <s6thgehr@gmail.com>
2023-01-17 09:25:25 +0100

    Remove nodewallet and use keypair person who is running script

--- packages/scripts/createGSolMetadata.ts
+++ packages/scripts/createGSolMetadata.ts
@@ -1,71 +1,36 @@
 import { SunriseStakeClient } from "@sunrisestake/app/src/lib/client";
 import "./util";
 import { SUNRISE_STAKE_STATE } from "@sunrisestake/app/src/lib/stakeAccount";
-import { AnchorProvider, setProvider } from "@project-serum/anchor";
 import * as anchor from "@project-serum/anchor";
-import {
-  clusterApiUrl,
-  Connection,
-  Keypair,
-  LAMPORTS_PER_SOL,
-} from "@solana/web3.js";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import {
   Metaplex,
-  keypairIdentity,
+  walletAdapterIdentity,
   bundlrStorage,
   toMetaplexFile,
 } from "@metaplex-foundation/js";
 import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
-import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 import * as fs from "fs";
-import { findGSolMintAuthority } from "@sunrisestake/app/src/lib/client/util";
+import {
+  findGSolMintAuthority,
+  setUpAnchor,
+} from "@sunrisestake/app/src/lib/client/util";
 
 const name = "Sunrise gSOL";
 const description = "Sunrise Stake Green SOL Token";
 const symbol = "GSOL";
 const imageFile = "gSOL.png";
 
-async function airdropSolIfNeeded(
-  signer: Keypair,
-  connection: Connection
-): Promise<void> {
-  const balance = await connection.getBalance(signer.publicKey);
-  console.log("Current balance is", balance / LAMPORTS_PER_SOL);
-
-  if (balance < LAMPORTS_PER_SOL) {
-    console.log("Airdropping 1 SOL...");
-    const airdropSignature = await connection.requestAirdrop(
-      signer.publicKey,
-      LAMPORTS_PER_SOL
-    );
-
-    const latestBlockHash = await connection.getLatestBlockhash();
-
-    await connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: airdropSignature,
-    });
-
-    const newBalance = await connection.getBalance(signer.publicKey);
-    console.log("New balance is", newBalance / LAMPORTS_PER_SOL);
-  }
-}
-
 console.log("Creating gSol metadata");
 
 (async () => {
   const connection = new Connection(clusterApiUrl("devnet"));
 
-  // This will be the payer of creating the metadata account
-  // TODO: Change this to be the keypair of update authority
-  const keypair = new Keypair();
-  const wallet = new NodeWallet(keypair);
-  await airdropSolIfNeeded(wallet.payer, connection);
+  const provider = setUpAnchor();
 
   const metaplex = Metaplex.make(connection)
-    .use(keypairIdentity(keypair))
+    .use(walletAdapterIdentity(provider.wallet))
     .use(
       bundlrStorage({
         address: "https://devnet.bundlr.network",
@@ -75,7 +40,7 @@ console.log("Creating gSol metadata");
     );
 
   // Upload image and get image uri
-  const buffer = fs.readFileSync("packages/app/public/" + imageFile);
+  const buffer = fs.readFileSync("packages/app/" + imageFile);
   const file = toMetaplexFile(buffer, imageFile);
 
   const imageUri = await metaplex.storage().upload(file);
@@ -89,9 +54,6 @@ console.log("Creating gSol metadata");
     symbol,
   });
   console.log("metadata uri:", uri);
-
-  const provider = new AnchorProvider(connection, wallet, {});
-  setProvider(provider);
 
   const client = await SunriseStakeClient.get(provider, SUNRISE_STAKE_STATE);
 
@@ -129,7 +91,7 @@ console.log("Creating gSol metadata");
       marinadeState: client.marinade.config.marinadeStateAddress,
       gsolMint: sunriseStakeState.gsolMint,
       gsolMintAuthority: gsolMintAuthority,
-      updateAuthority: sunriseStakeState.updateAuthority,
+      updateAuthority: sunriseStakeState.updateAuthority, // should be equal to the one who is running the script?
       tokenProgram: TOKEN_PROGRAM_ID,
       metadata: metadataAddress,
       tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
@@ -137,7 +99,4 @@ console.log("Creating gSol metadata");
     .rpc();
 
   console.log(transactionSignature);
-
-  const balance = await connection.getBalance(wallet.publicKey);
-  console.log("Current balance is", balance / LAMPORTS_PER_SOL);
 })().catch(console.error);
