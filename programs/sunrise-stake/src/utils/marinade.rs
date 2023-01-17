ad133edf765d6933081ceee79ba69ae3e431d82f
crustydev <ademola1145@gmail.com>
2023-01-17 02:38:07 +0100

    Include calculation for bSOL value in lamports

--- programs/sunrise-stake/src/utils/marinade.rs
+++ programs/sunrise-stake/src/utils/marinade.rs
@@ -328,6 +328,18 @@ pub fn recoverable_yield<'a>(
     Ok(recoverable_msol)
 }
 
+#[allow(dead_code)]
+pub fn bsol_value_in_lamports<'a>(
+    blaze_stake_pool: &AccountInfo,
+    bsol_token_account: &Account<'a, TokenAccount>,
+) -> Result<u64> {
+    let stake_pool = spl_stake_pool::state::StakePool::try_from_slice(
+        &blaze_stake_pool.data.borrow())?;
+    let bsol_balance = bsol_token_account.amount;
+    
+    proportional(bsol_balance, stake_pool.total_lamports, stake_pool.pool_token_supply)
+}
+
 pub fn get_delegated_stake_amount<'a>(stake_account: &AccountInfo<'a>) -> Result<u64> {
     // Gets the active stake amount of the stake account. We need this to determine how much gSol to mint.
     let stake_account_state =
