b651d3a5c35938417334aeff07e4882598af0352
Thomas Gehrmann <s6thgehr@gmail.com>
2023-02-02 21:54:43 +0100

    Fix tooltips in details panel

--- packages/app/src/components/TooltipPopover.tsx
+++ packages/app/src/components/TooltipPopover.tsx
@@ -34,7 +34,7 @@ const TooltipPopover: FC<TooltipProps> = ({ children }) => {
         ref={setPopperElement}
         style={styles.popper}
         {...attributes.popper}
-        className="bg-green px-4 py-2 rounded-md text-xs min-w-[120px] sm:min-w-[220px] w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 relative z-50"
+        className="bg-green px-4 py-2 rounded-md text-xs min-w-[120px] sm:min-w-[160px] w-1/2 md:w-1/3 relative z-50"
       >
         {children}
         <div ref={setArrowElement} style={styles.arrow} />
