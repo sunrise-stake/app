78c73f7e8b3f342d07707679b86f0c53e0df0c3b
Thomas Gehrmann <100349684+s6thgehr@users.noreply.github.com>
2023-02-02 19:51:37 +0100

    Update packages/app/src/utils/tooltips.ts
    Co-authored-by: Daniel Kelleher <dankelleher@yahoo.com>

--- packages/app/src/utils/tooltips.ts
+++ packages/app/src/utils/tooltips.ts
@@ -1,5 +1,5 @@
 export const tooltips = {
-  yourStake: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+  yourStake: `Your total staked SOL`
   eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
   At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
   no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
