1025c12c8b6b3913f59b07370bce76f8261dc76f
Thomas Gehrmann <100349684+s6thgehr@users.noreply.github.com>
2023-02-02 19:51:47 +0100

    Update packages/app/src/utils/tooltips.ts
    Co-authored-by: Daniel Kelleher <dankelleher@yahoo.com>

--- packages/app/src/utils/tooltips.ts
+++ packages/app/src/utils/tooltips.ts
@@ -4,7 +4,7 @@ export const tooltips = {
   At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
   no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
 
-  totalStatke: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+  totalStatke: `The sum of everyone's staked SOL`
   sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
   erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea 
   rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
