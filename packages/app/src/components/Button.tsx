8ea6bf17b4b14bc4afb308b51a9bd6ad8bf77e03
Ronny Haase <ronnyhaase@fastmail.com>
2023-02-04 00:02:37 +0100

    Make buttons center content verically

--- packages/app/src/components/Button.tsx
+++ packages/app/src/components/Button.tsx
@@ -18,7 +18,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       className={clx(
-        "inline-flex rounded-lg leading-6 text-white text-xl shadow-sm disabled:brightness-75 hover:brightness-75",
+        "inline-flex items-center rounded-lg leading-6 text-white text-xl shadow-sm disabled:brightness-75 hover:brightness-75",
         {
           "bg-green": variant === "primary",
           "bg-danger": variant === "danger",
