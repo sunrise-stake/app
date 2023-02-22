e1b07ca2660ca0f3313b48cdeb99e4a59747415f
Ronny Haase <ronnyhaase@fastmail.com>
2023-02-22 21:40:20 +0100

    Use Route(s) instead of createBrowserRouter for easier transitions

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -12,32 +12,12 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import React, { type FC, useMemo } from "react";
-import { RouterProvider, createBrowserRouter, Link } from "react-router-dom";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import { SunriseProvider } from "./common/context/sunriseStakeContext";
 import { StakingApp } from "./staking/StakingApp";
 require("./solana-wallet-adapter.css");
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <StakingApp />,
-    errorElement: (
-      <div className="flex flex-col min-h-screen justify-center items-center">
-        <p className="text-2xl font-bold">
-          Oh, oh. You&apos;ve got lost in the woods... 🐺
-        </p>
-        <Link
-          className="mt-2 px-5 py-3 border-2 border-green rounded-lg leading-6 text-green text-xl"
-          to="/"
-        >
-          Back home
-        </Link>
-      </div>
-    ),
-  },
-]);
 
 const App: FC = () => {
   const network =
@@ -54,16 +34,35 @@ const App: FC = () => {
     ],
     []
   );
+  const location = useLocation();
 
   return (
     <>
       <div className="App min-h-screen text-white">
         <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect>
+          <WalletProvider wallets={wallets}>
             <WalletModalProvider>
               <SunriseProvider>
                 <Toaster />
-                <RouterProvider router={router} />
+                <Routes location={location}>
+                  <Route path="/" element={<StakingApp />} />
+                  <Route
+                    path="/*"
+                    element={
+                      <div className="flex flex-col min-h-screen justify-center items-center">
+                        <p className="text-2xl font-bold">
+                          Oh, oh. You&apos;ve got lost in the woods... 🐺
+                        </p>
+                        <Link
+                          className="mt-2 px-5 py-3 border-2 border-green rounded-lg leading-6 text-green text-xl"
+                          to="/"
+                        >
+                          Back home
+                        </Link>
+                      </div>
+                    }
+                  />
+                </Routes>
               </SunriseProvider>
             </WalletModalProvider>
           </WalletProvider>
