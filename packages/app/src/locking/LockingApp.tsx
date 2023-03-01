cbead0b37904cca93029a05953b8ac861f9681d9
Ronny Haase <ronnyhaase@fastmail.com>
2023-03-01 13:00:59 +0100

    Add invalid link to demo

--- packages/app/src/locking/LockingApp.tsx
+++ packages/app/src/locking/LockingApp.tsx
@@ -1,15 +1,19 @@
 import clx from "classnames";
 import { type FC } from "react";
+import { Link } from "react-router-dom";
 
 const LockingApp: FC<
   { className?: string } & React.HTMLAttributes<HTMLElement>
 > = ({ className, ...rest }) => {
   return (
     <div
-      className={clx("flex justify-center items-center", className)}
+      className={clx("flex flex-col justify-center items-center", className)}
       {...rest}
     >
       <h2>Lock.</h2>
+      <Link to="invalid" className="block text-green">
+        Invalid link...
+      </Link>
     </div>
   );
 };
