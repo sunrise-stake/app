ca3e9770b629f0ad3138665351866e84b34e515c
Thomas Gehrmann <s6thgehr@gmail.com>
2023-03-01 16:47:37 +0100

    Style sending gSol form

--- packages/app/src/grow/components/SendGSolForm.tsx
+++ packages/app/src/grow/components/SendGSolForm.tsx
@@ -1,6 +1,6 @@
 import clx from "classnames";
 import { useCallback, useState, type FC } from "react";
-import { Button, Spinner } from "../../common/components";
+import { AmountInput, Button, Spinner } from "../../common/components";
 import { GiPresent } from "react-icons/gi";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { PublicKey, Transaction } from "@solana/web3.js";
@@ -10,9 +10,14 @@ import {
   getAssociatedTokenAddress,
   getAccount,
 } from "@solana/spl-token";
-import { solToLamports } from "../../common/utils";
+import { solToLamports, ZERO } from "../../common/utils";
 import { type Details } from "@sunrisestake/client";
 import { useSunriseStake } from "../../common/context/sunriseStakeContext";
+import BN from "bn.js";
+import {
+  NotificationType,
+  notifyTransaction,
+} from "../../common/components/notifications";
 
 interface Props {
   className?: string;
@@ -22,6 +27,7 @@ const SendGSolForm: FC<Props> = ({ className }) => {
   const [amount, setAmount] = useState("");
   const [recipientAddress, setRecipientAddress] = useState("");
   const [isBusy, setIsBusy] = useState(false);
+  const [isValid, setIsValid] = useState(false);
 
   const {
     details,
@@ -34,11 +40,27 @@ const SendGSolForm: FC<Props> = ({ className }) => {
   console.log("Connection:", connection);
   console.log("Sender:", senderPubkey?.toBase58());
 
+  const handleError = (error: Error): void => {
+    notifyTransaction({
+      type: NotificationType.error,
+      message: "Transaction failed",
+      description: error.message,
+    });
+    console.error(error);
+  };
+
   const transferGSol = useCallback(async () => {
     if (!senderPubkey || !details) {
       return;
     }
-    const recipientPubkey = new PublicKey(recipientAddress);
+    let recipientPubkey;
+    try {
+      recipientPubkey = new PublicKey(recipientAddress);
+    } catch (e) {
+      handleError(e as Error);
+      console.log(e);
+      return;
+    }
     const mint = new PublicKey(details.sunriseStakeConfig.gsolMint);
 
     const transaction = new Transaction();
@@ -72,28 +94,36 @@ const SendGSolForm: FC<Props> = ({ className }) => {
         solToLamports(amount).toNumber()
       )
     );
-    const signature = await sendTransaction(transaction, connection);
+    const signature = sendTransaction(transaction, connection)
+      .then((tx) => {
+        console.log(tx);
+      })
+      .catch(handleError);
     console.log("Transfer signature:", signature);
   }, [recipientAddress]);
 
   return (
     <div
       className={clx(
-        "bg-gray-300 backdrop-blur-sm px-8 py-4 rounded-md w-full sm:w-[50%]",
+        "bg-inset bg-opacity-10 backdrop-blur-sm px-8 py-4 rounded-md w-full sm:w-[60%] md:w-[40%]",
         className
       )}
     >
       <div className="flex flex-col">
-        <div className="font-semibold text-xl">Send</div>
-        <input
-          onChange={(e) => {
-            setAmount(e.target.value);
-          }}
-          value={amount}
+        <div className="font-semibold text-xl mb-2">Send gSOL</div>
+        <AmountInput
+          className="mb-5"
+          token="gSOL"
+          balance={new BN(details?.balances.gsolBalance.amount ?? ZERO)}
+          amount={amount}
+          setAmount={setAmount}
+          setValid={setIsValid}
+          mode="UNSTAKE"
+          variant="small"
         />
-        <div className="font-semibold text-xl">To</div>
+        <div className="font-semibold text-xl mb-2">To</div>
         <input
-          className="mb-4"
+          className="mb-4 rounded-md text-lg py-3 px-4"
           onChange={(e) => {
             setRecipientAddress(e.target.value);
           }}
@@ -106,7 +136,7 @@ const SendGSolForm: FC<Props> = ({ className }) => {
               setIsBusy(false);
             });
           }}
-          disabled={isBusy}
+          disabled={isBusy || !isValid}
         >
           <div className="flex gap-2 w-full justify-center items-center">
             {isBusy ? (
