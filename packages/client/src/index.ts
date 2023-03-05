2c4e70d2c8a80ec4998d668f5bfaaf2e56d4c731
dankelleher <dankelleher@yahoo.com>
2023-03-05 18:04:53 +0100

    Lock&Mint, fix CI issues

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -442,9 +442,13 @@ export class SunriseStakeClient {
       throw new Error("No epoch report account found during recoverTickets");
     }
 
+    this.log(`Current epoch: ${currentEpoch.epoch}, epoch report epoch: ${epochReport.epoch.toNumber()}`)
     if (currentEpoch.epoch === epochReport.epoch.toNumber()) {
       // nothing to do here - the report account is for the current epoch, so we cannot recover any tickets yet
+      this.log("Skipping recoverTickets, epoch report is for current epoch")
       return null;
+    } else {
+      this.log("Updating epoch report account and recovering tickets from previous epoch")
     }
 
     // get a list of all the open delayed unstake tickets that can now be recovered
--- programs/sunrise-stake/src/instructions/trigger_pool_rebalance.rs
+++ programs/sunrise-stake/src/instructions/trigger_pool_rebalance.rs
@@ -2,6 +2,7 @@ use crate::state::{EpochReportAccount, State};
 use crate::utils::marinade;
 use crate::utils::seeds::{EPOCH_REPORT_ACCOUNT, MSOL_ACCOUNT, ORDER_UNSTAKE_TICKET_ACCOUNT};
 use crate::utils::system;
+use crate::error::ErrorCode;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount};
 use marinade_cpi::program::MarinadeFinance;
@@ -96,7 +97,7 @@ pub struct TriggerPoolRebalance<'info> {
     mut,
     seeds = [state.key().as_ref(), EPOCH_REPORT_ACCOUNT],
     bump = epoch_report_account.bump,
-    constraint = epoch == clock.epoch
+    constraint = epoch == clock.epoch @ ErrorCode::InvalidEpochReportAccount,
     )]
     pub epoch_report_account: Box<Account<'info, EpochReportAccount>>,
 
