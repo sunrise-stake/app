f5c5f2e734b74f31e71c9fe717b0d0b38d808f59
dankelleher <dankelleher@yahoo.com>
2023-03-06 17:46:28 +0100

    Update to use yield controller total tokens purchased instead of holding amount in useCarbon. (allowing SOL to be removed from the holding amount without affecting historical values)

--- packages/app/src/common/hooks/useCarbon.ts
+++ packages/app/src/common/hooks/useCarbon.ts
@@ -4,9 +4,11 @@ import { useEffect, useState } from "react";
 
 import { useSunriseStake } from "../context/sunriseStakeContext";
 import { solToCarbon } from "../utils";
+import { useYieldController } from "./useYieldController";
 
 const useCarbon = (): { totalCarbon: number | undefined } => {
   const { details } = useSunriseStake();
+  const yieldControllerState = useYieldController();
   const [totalCarbon, setTotalCarbon] = useState<number>();
 
   useEffect(() => {
@@ -16,26 +18,34 @@ const useCarbon = (): { totalCarbon: number | undefined } => {
       // Total carbon is the carbon value of
       // 1. the extractable yield
       // 2. the treasury balance
-      // 3. the holding account balance
-      // (TODO this last one will be replaced with the TreasuryController total_spent value)
+      // 3. the YieldController totalTokensPurchased value
 
       const extractableYield = details.extractableYield;
       const treasuryBalance = new BN(details.balances.treasuryBalance);
+      // no longer used to calculate the amount - can be removed
       const holdingAccountBalance = new BN(
         details.balances.holdingAccountBalance
       );
+      // this is the amount of carbon tokens burned so far by the protocol
+      const totalTokensPurchased =
+        yieldControllerState?.totalTokensPurchased ?? new BN(0);
 
-      const totalLamports = extractableYield
-        .add(treasuryBalance)
-        .add(holdingAccountBalance);
+      // this is the current price set in the yield controller for buying carbon tokens
+      // TODO use this instead of the hard-coded values to convert SOL to Carbon
+      // const price = yieldControllerState?.price ?? 0;
 
-      const carbon = solToCarbon(toSol(totalLamports));
+      const totalLamportsWaiting = extractableYield.add(treasuryBalance);
+
+      const carbon =
+        solToCarbon(toSol(totalLamportsWaiting)) +
+        totalTokensPurchased.toNumber();
 
       console.log({
         extractableYield: toSol(extractableYield),
         treasuryBalance: toSol(treasuryBalance),
         holdingAccountBalance: toSol(holdingAccountBalance),
-        totalLamports: toSol(totalLamports),
+        totalLamportsWaiting: toSol(totalLamportsWaiting),
+        totalTokensPurchased: totalTokensPurchased.toNumber(),
         totalCarbon: carbon,
       });
 
