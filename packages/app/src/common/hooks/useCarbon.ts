7ee91d14fd9f3c89a7bff219a42bcc8c1bf6f87f
dankelleher <dankelleher@yahoo.com>
2023-03-07 14:18:46 +0100

    Fix co2 reporting

--- packages/app/src/common/hooks/useCarbon.ts
+++ packages/app/src/common/hooks/useCarbon.ts
@@ -28,7 +28,8 @@ const useCarbon = (): { totalCarbon: number | undefined } => {
       );
       // this is the amount of carbon tokens burned so far by the protocol
       const totalTokensPurchased =
-        yieldControllerState?.totalTokensPurchased ?? new BN(0);
+        (yieldControllerState?.totalTokensPurchased ?? new BN(0)).toNumber() /
+        10 ** 8; // tokens purchased are stored on-chain in minor denomination
 
       // this is the current price set in the yield controller for buying carbon tokens
       // TODO use this instead of the hard-coded values to convert SOL to Carbon
@@ -37,15 +38,14 @@ const useCarbon = (): { totalCarbon: number | undefined } => {
       const totalLamportsWaiting = extractableYield.add(treasuryBalance);
 
       const carbon =
-        solToCarbon(toSol(totalLamportsWaiting)) +
-        totalTokensPurchased.toNumber();
+        solToCarbon(toSol(totalLamportsWaiting)) + totalTokensPurchased;
 
       console.log({
         extractableYield: toSol(extractableYield),
         treasuryBalance: toSol(treasuryBalance),
         holdingAccountBalance: toSol(holdingAccountBalance),
         totalLamportsWaiting: toSol(totalLamportsWaiting),
-        totalTokensPurchased: totalTokensPurchased.toNumber(),
+        totalTokensPurchased,
         totalCarbon: carbon,
       });
 
@@ -54,7 +54,7 @@ const useCarbon = (): { totalCarbon: number | undefined } => {
 
       setTotalCarbon(normalizedCarbon);
     })();
-  }, [details]);
+  }, [details, yieldControllerState]);
 
   return { totalCarbon };
 };
