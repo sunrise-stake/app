e61f35f6ebf815e21d2e5776803f1dd9822b1050
dankelleher <dankelleher@yahoo.com>
2023-03-11 09:05:30 +0100

    More grow screen styling

---
+++ packages/app/src/common/components/modals/SendGSolModal.tsx
@@ -0,0 +1,167 @@
+import React, { type FC, useCallback, useState } from "react";
+
+import { BaseModal, type ModalProps } from "./";
+import { PublicKey, Transaction } from "@solana/web3.js";
+import clx from "classnames";
+import { AmountInput } from "../AmountInput";
+import BN from "bn.js";
+import { handleError, solToLamports, ZERO } from "../../utils";
+import { Button } from "../Button";
+import { Spinner } from "../Spinner";
+import { GiPresent } from "react-icons/gi";
+import { useSunriseStake } from "../../context/sunriseStakeContext";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  createAssociatedTokenAccountInstruction,
+  createTransferInstruction,
+  getAccount,
+  getAssociatedTokenAddress,
+} from "@solana/spl-token";
+import { NotificationType, notifyTransaction } from "../notifications";
+
+interface SendGSolModalProps {
+  recipient?: {
+    address: PublicKey;
+    name?: string;
+    imageUrl?: string;
+    website?: string;
+  };
+  className?: string;
+}
+const SendGSolModal: FC<ModalProps & SendGSolModalProps> = ({
+  className = "",
+  recipient: recipientFromProps,
+  ...props
+}) => {
+  const [amount, setAmount] = useState("");
+
+  const [isBusy, setIsBusy] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+
+  const { details } = useSunriseStake();
+  const { publicKey: senderPubkey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+  const [recipient, setRecipient] = useState(recipientFromProps);
+
+  const updateRecipientFromForm = useCallback(
+    (addressString: string) => {
+      setRecipient({
+        address: new PublicKey(addressString),
+      });
+    },
+    [setRecipient]
+  );
+
+  const transferGSol = useCallback(async (): Promise<void> => {
+    if (!senderPubkey || !details || !recipient) {
+      return;
+    }
+    const mint = new PublicKey(details.sunriseStakeConfig.gsolMint);
+
+    const transaction = new Transaction();
+    const associatedTokenFrom = await getAssociatedTokenAddress(
+      mint,
+      senderPubkey
+    );
+    const fromAccount = await getAccount(connection, associatedTokenFrom);
+
+    const associatedTokenTo = await getAssociatedTokenAddress(
+      mint,
+      recipient.address
+    );
+
+    if (!(await connection.getAccountInfo(associatedTokenTo))) {
+      transaction.add(
+        createAssociatedTokenAccountInstruction(
+          senderPubkey,
+          associatedTokenTo,
+          recipient.address,
+          mint
+        )
+      );
+    }
+
+    transaction.add(
+      createTransferInstruction(
+        fromAccount.address,
+        associatedTokenTo,
+        senderPubkey,
+        solToLamports(amount).toNumber()
+      )
+    );
+    const signature = sendTransaction(transaction, connection)
+      .then((tx) => {
+        notifyTransaction({
+          type: NotificationType.success,
+          message: "Transfer successful",
+          txid: tx,
+        });
+      })
+      .catch(handleError);
+    console.log("Transfer signature:", signature);
+  }, [recipient, amount, senderPubkey, details, connection, sendTransaction]);
+
+  return (
+    <BaseModal {...props}>
+      <div
+        className={clx(
+          "bg-inset bg-opacity-10 backdrop-blur-sm px-8 py-4 rounded-md",
+          className
+        )}
+      >
+        <div className="flex flex-col">
+          <div className="font-semibold text-xl mb-2">
+            To{" "}
+            <span className="font-normal text-lg text-green">
+              {recipient?.name ?? recipient?.address.toBase58()}
+            </span>
+          </div>
+          <input
+            className="mb-4 rounded-md text-sm xl:text-lg py-3 px-4 placeholder:text-sm"
+            onChange={(e) => {
+              updateRecipientFromForm(e.target.value);
+            }}
+            value={recipient?.address.toBase58() ?? ""}
+            placeholder="Address"
+          />
+          <div className="font-semibold text-xl mb-2">Send gSOL</div>
+          <div className="flex items-center gap-4">
+            <AmountInput
+              className="basis-3/4"
+              token="gSOL"
+              balance={new BN(details?.balances.gsolBalance.amount ?? ZERO)}
+              amount={amount}
+              setAmount={setAmount}
+              setValid={setIsValid}
+              mode="UNSTAKE"
+              variant="small"
+            />
+
+            <Button
+              className="basis-1/4"
+              onClick={() => {
+                setIsBusy(true);
+                transferGSol().finally(() => {
+                  setIsBusy(false);
+                });
+              }}
+              disabled={isBusy || !isValid}
+              size="sm"
+            >
+              <div className="flex gap-2 w-full justify-center items-center">
+                {isBusy ? (
+                  <Spinner size="1rem" className="mr-1" />
+                ) : (
+                  <GiPresent size={32} />
+                )}
+                Send
+              </div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </BaseModal>
+  );
+};
+
+export { SendGSolModal };
