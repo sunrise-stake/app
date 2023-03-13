755d48250630e1288b906f2d333523b425226f16
Ronny Haase <ronnyhaase@fastmail.com>
2023-03-13 22:43:05 +0100

    Fix fade in of guide popup
    Fade out still a mystery...

--- packages/app/src/common/components/modals/PageHelpModal.tsx
+++ packages/app/src/common/components/modals/PageHelpModal.tsx
@@ -8,28 +8,36 @@ const PageHelpModal: FC<{ show: boolean; onClose?: () => void }> = ({
   onClose = noop,
 }) => {
   return (
-    <Transition
-      as={Fragment}
-      show={show}
-      enterFrom="opacity-0"
-      enterTo="opacity-100"
-      enter="transition-opacity ease-in duration-500"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-      leave="transition-opacity ease-out duration-200"
-    >
-      <Dialog
-        className="fixed z-30 inset-0 overflow-y-auto backdrop-blur-sm"
-        onClose={onClose}
-        open={show}
-      >
-        <div className="flex items-center h-full">
-          <Dialog.Panel className="relative h-3/4 w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/2 mx-auto overflow-hidden rounded-lg p-2 border border-green text-left bg-white">
-            <div className="absolute top-1 right-4 text-right text-green">
-              <button onClick={onClose}>x</button>
-            </div>
-            <GuideSelector />
-          </Dialog.Panel>
+    <Transition as={Fragment} show={show}>
+      <Dialog className="relative z-30" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enterFrom="opacity-0 backdrop-blur-0"
+          enterTo="opacity-100 backdrop-blur-sm"
+          enter="ease-in duration-200"
+          leaveFrom="opacity-100 backdrop-blur-sm"
+          leaveTo="opacity-0 backdrop-blur-0"
+          leave="ease-out duration-200"
+        >
+          <div className="fixed inset-0" aria-hidden="true"></div>
+        </Transition.Child>
+        <div className="fixed inset-0 flex items-center h-full">
+          <Transition.Child
+            as={Fragment}
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            enter="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            leave="ease-out duration-200"
+          >
+            <Dialog.Panel className="relative h-3/4 w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/2 mx-auto overflow-hidden rounded-lg p-2 border border-green text-left bg-white">
+              <div className="absolute top-1 right-4 text-right text-green">
+                <button onClick={onClose}>x</button>
+              </div>
+              <GuideSelector />
+            </Dialog.Panel>
+          </Transition.Child>
         </div>
       </Dialog>
     </Transition>
