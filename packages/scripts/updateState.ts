25695a821ca7c938b5bdafd5eb90e933698c1b82
dankelleher <dankelleher@yahoo.com>
2023-03-31 08:43:42 +0200

    Update Devnet env to use new Buy&Burn program. Note - in the devnet buy&burn state, the switchboard price oracle feed addresses are wrong, they are just copied from mainnet. Switchboard was not working in devnet at the time.

--- packages/scripts/updateState.ts
+++ packages/scripts/updateState.ts
@@ -17,6 +17,9 @@ import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
         verbose: true,
       });
   await client.update({
-    newTreasury: new PublicKey("E7BjB9UQp814RsMPq7U6S4fy6wRzn6tFTYt31kJoskoq"),
+      // dev
+      newTreasury: new PublicKey("8Z8BwUqZTD8XyJ6sdEEeWLaZ9Bg1J7TRDoYPfhkGS9Hr"),
+      // prod
+    // newTreasury: new PublicKey("E7BjB9UQp814RsMPq7U6S4fy6wRzn6tFTYt31kJoskoq"),
   });
 })().catch(console.error);
