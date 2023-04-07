8a21c59473ab7ab38a4438afb45d684e4f031d89
Ronny Haase <ronnyhaase@fastmail.com>
2023-04-08 00:56:48 +0200

    Add basic Tip Jar page hero

--- packages/app/src/tipjar/TipjarApp.tsx
+++ packages/app/src/tipjar/TipjarApp.tsx
@@ -6,6 +6,10 @@ import { AppRoute } from "../Routes";
 import { useHelp } from "../common/context/HelpContext";
 import { Link } from "react-router-dom";
 import { IoChevronBackOutline } from "react-icons/io5";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { toShortBase58 } from "../common/utils";
+import { FaWallet } from "react-icons/fa";
 // import { useLocation, useNavigate } from "react-router-dom";
 // import { useWallet } from "@solana/wallet-adapter-react";
 
@@ -18,7 +22,7 @@ const _TipjarApp: ForwardRefRenderFunction<
 
   // const location = useLocation();
   // const navigate = useNavigate();
-  // const wallet = useWallet();
+  const wallet = useWallet();
   // useEffect(() => {
   //   if (!wallet.connected && location.state?.address === undefined)
   //     navigate("/");
@@ -28,9 +32,9 @@ const _TipjarApp: ForwardRefRenderFunction<
     if (currentHelpRoute !== AppRoute.TipJar) return; // we are not on the stake page, so don't update zen mode
     updateZenMode({
       showBGImage: false,
-      showHelpButton: true,
+      showHelpButton: false,
       showExternalLinks: false,
-      showWallet: active,
+      showWallet: false,
     });
   }, [active]);
 
@@ -40,13 +44,40 @@ const _TipjarApp: ForwardRefRenderFunction<
       ref={ref}
       {...rest}
     >
-      <div className="container">
+      <div className="container mt-5">
         <Link to="/grow" className="flex items-center text-green">
           <div className="flex items-center nowrap">
             <IoChevronBackOutline className="inline" size={48} />
           </div>
         </Link>
-        <h1>Drip x Sunrise</h1>
+        <h2 className="my-4 text-center text-3xl">
+          <span className="text-green">Sunrise Stake</span> x{" "}
+          <span className="text-[#8559D5]">DRiP</span>
+        </h2>
+      </div>
+      <div className="w-full p-8 bg-green text-white">
+        <h3 className="mb-2 text-3xl text-ticket">Earth Day</h3>
+        <div className="mb-4 text-lg">
+          <p>DRiP sends you wonderful and free NFTs every week.</p>
+          <p>
+            For Earth Day, Sunrise Stake and DRiP want to give you the chance to
+            appreciate the artists work and offset carbon by doing so.
+            <br />
+            Send some green SOL as a thank you to the artist of your DRiP drop.
+          </p>
+        </div>
+        <div className="my-2">
+          <WalletMultiButton className="!bg-ticket !text-[#145D3E] !text-sm">
+            {wallet.publicKey ? (
+              <>
+                <FaWallet className="mr-2" />
+                {toShortBase58(wallet.publicKey)}
+              </>
+            ) : (
+              "Connect wallet"
+            )}
+          </WalletMultiButton>
+        </div>
       </div>
     </div>
   );
