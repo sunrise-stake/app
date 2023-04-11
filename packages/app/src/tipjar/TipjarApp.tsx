2c149286d3c2e93ba750cbe6328f51e9e92aa9b5
Thomas Gehrmann <s6thgehr@gmail.com>
2023-04-11 17:58:13 +0200

    Implement hero section

--- packages/app/src/tipjar/TipjarApp.tsx
+++ packages/app/src/tipjar/TipjarApp.tsx
@@ -10,6 +10,7 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { toShortBase58 } from "../common/utils";
 import { FaWallet } from "react-icons/fa";
+import { Button } from "../common/components";
 // import { useLocation, useNavigate } from "react-router-dom";
 // import { useWallet } from "@solana/wallet-adapter-react";
 
@@ -47,33 +48,55 @@ const _TipjarApp: ForwardRefRenderFunction<
             <IoChevronBackOutline className="inline" size={48} />
           </div>
         </Link>
-        <h2 className="my-4 text-center text-3xl">
-          <span className="text-green">Sunrise Stake</span> x{" "}
-          <span className="text-[#8559D5]">DRiP</span>
-        </h2>
-      </div>
-      <div className="w-full p-8 bg-green text-white">
-        <h3 className="mb-2 text-3xl text-ticket">Earth Day</h3>
-        <div className="mb-4 text-lg">
-          <p>DRiP sends you wonderful and free NFTs every week.</p>
-          <p>
-            For Earth Day, Sunrise Stake and DRiP want to give you the chance to
-            appreciate the artists work and offset carbon by doing so.
-            <br />
-            Send some green SOL as a thank you to the artist of your DRiP drop.
-          </p>
+        <div className="flex flex-row justify-between">
+          <h2 className="my-4 text-center text-3xl">
+            <img src="logo.png" width="40" className="inline mr-4" />
+            <span className="text-green">Sunrise Stake</span> x{" "}
+            <span className="text-[#8559D5]">DRiP</span>
+          </h2>
+          <div className="my-2">
+            <WalletMultiButton className="!bg-[#145D3E] !text-white !text-sm">
+              {wallet.publicKey ? (
+                <>
+                  <FaWallet className="mr-2" />
+                  {toShortBase58(wallet.publicKey)}
+                </>
+              ) : (
+                "Connect wallet"
+              )}
+            </WalletMultiButton>
+          </div>
         </div>
-        <div className="my-2">
-          <WalletMultiButton className="!bg-ticket !text-[#145D3E] !text-sm">
-            {wallet.publicKey ? (
-              <>
-                <FaWallet className="mr-2" />
-                {toShortBase58(wallet.publicKey)}
-              </>
-            ) : (
-              "Connect wallet"
-            )}
-          </WalletMultiButton>
+      </div>
+
+      <div
+        className="w-full p-8 bg-cover text-white"
+        style={{ backgroundImage: "url('earth_day/hero_background.png')" }}
+      >
+        <div className="flex flex-row">
+          <div className="basis-1/2">
+            <h3 className="mb-2 text-5xl text-white p-8">Earth Day</h3>
+            <div className="mb-16 text-lg px-8">
+              <p>DRiP sends you wonderful and free NFTs every week.</p>
+              <p>
+                For Earth Day, Sunrise Stake and DRiP want to give you the
+                chance to appreciate the artists work and offset carbon by doing
+                so.
+                <br />
+                Send some green SOL as a thank you to the artist of your DRiP
+                drop.
+              </p>
+            </div>
+            <Button color="ticket" className="mx-8">
+              See available NFTs
+            </Button>
+            <Button color="ticket" variant="outline">
+              <a href="https://drip.haus/">Visit Drip&apos;s webpage</a>
+            </Button>
+          </div>
+          <div className="basis-1/2">
+            <img src="earth_day/hero_picture.png" className="m-auto py-8" />
+          </div>
         </div>
       </div>
     </div>
