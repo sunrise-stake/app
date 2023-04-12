dad1a1be2d4088817d0ae91accf3a1be64979383
Thomas Gehrmann <s6thgehr@gmail.com>
2023-04-13 00:04:07 +0200

    Remove redundant tailwind class

--- packages/app/src/grow/OrgButtonContent.tsx
+++ packages/app/src/grow/OrgButtonContent.tsx
@@ -2,7 +2,7 @@ import React, { type FC, type PropsWithChildren } from "react";
 
 export const OrgButtonContent: FC<PropsWithChildren> = ({ children }) => (
   <div
-    className="p-8 rounded-md w-40 h-30 text-white font-extrabold text-xl font-medium text-center"
+    className="p-8 rounded-md w-40 h-30 text-white text-xl font-medium text-center"
     style={{
       textShadow: "1px 2px 3px rgb(0 0 0 / 80%)",
     }}
