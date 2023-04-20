9a8416b6516d69b867fc28e878ae97a8f0d16f72
Ronny Haase <ronnyhaase@fastmail.com>
2023-04-20 19:22:36 +0200

    Add DRiP drop date to copy

--- packages/app/src/tipjar/components/DonatableArtistNFT.tsx
+++ packages/app/src/tipjar/components/DonatableArtistNFT.tsx
@@ -72,14 +72,14 @@ export const DonatableArtistNFT: FC<Props> = ({ query }) => {
       {nfts.length === 0 ? (
         <div className="container mb-12 text-center">
           <img className="inline" src="earth_day/tipjar.png" />
-          <h1 className="my-4 text-2xl">
+          <h1 className="my-4 text-3xl">
             Recognition for those who deserve it.
           </h1>
           <p>
-            Once you received your Earth Day DRiP NFT, you&apos;ll be able to
-            drop some SOL in your favourite artist&apos;s wallet. DRiP is all
-            about free art, so there&apos;s no obligation. Every lamport is
-            appreciated!
+            Once you received your Earth Day Sunrise x DRiP NFT on{" "}
+            <u>22nd of April</u>, you&apos;ll be able to drop some SOL in your
+            favourite artist&apos;s wallet. DRiP is all about free art, so
+            there&apos;s no obligation. Every lamport is appreciated!
           </p>
         </div>
       ) : null}
