53e9020018b23e39d38ea82b56822cb8a44d6d86
Thomas Gehrmann <s6thgehr@gmail.com>
2023-10-19 17:26:31 +0200

    Create water bowl and balance components

---
+++ packages/app/src/yield/AccountBalance.tsx
@@ -0,0 +1,20 @@
+import { type FC } from "react";
+import WaterBowl from "./WaterBowl";
+
+interface AccountBalanceProps {
+  title: string;
+  balance: string;
+}
+const AccountBalance: FC<AccountBalanceProps> = ({ title, balance }) => {
+  return (
+    <div className="flex gap-2 my-8 justify-center">
+      <div className="text-2xl font-bold w-40">{title}</div>
+      <div className="flex flex-col">
+        <WaterBowl />
+        <div className="text-lg">{balance} Sol</div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountBalance;
--- packages/app/src/yield/YieldApp.tsx
+++ packages/app/src/yield/YieldApp.tsx
@@ -1,22 +1,20 @@
 import { forwardRef, type ForwardRefRenderFunction } from "react";
+import AccountBalance from "./AccountBalance";
 
 const _YieldApp: ForwardRefRenderFunction<
   HTMLDivElement,
   { className?: string; active?: boolean } & React.HTMLAttributes<HTMLElement>
 > = ({ className, active = false, ...rest }, ref) => {
   return (
-    <div className="flex flex-col justify-startq w-full mt-16">
-      <div className="flex justify-center">
-        <div>Sunrise Stake</div>
-        <div>34 Sol</div>
-      </div>
-      <div className="flex justify-center">
-        <div>Yield router</div>
-        <div>4 Sol</div>
-      </div>
-      <div className="flex justify-center">
-        <div>Eco token</div>
-        <div>4 Sol</div>
+    <div className="flex flex-col justify-start w-full mt-16">
+      <AccountBalance title="Sunrise Stake" balance="45" />
+      <AccountBalance title="Yield Router" balance="5" />
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col">
+          <AccountBalance title="Eco Token Escrow" balance="0.8" />
+          <AccountBalance title="Eco Token" balance="0.8" />
+        </div>
+        <AccountBalance title="Offset Bridge" balance="15" />
       </div>
     </div>
   );
